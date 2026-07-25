c8bcc06c50ca2b1896c3e1a34fd855f74eecee35
e <e@e>
2026-07-25 10:38:15 -0500

    Conecta la implementación final de Apps Script

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.ATELIER_CONFIG = {
   // Pega aquí la URL /exec de tu despliegue de Google Apps Script.
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyO2svWMYFTaAvLsX9MiqxX3Z54P___iAG0sZItdWr2p7Dt2Y5qMJXQq2bAaq7Q5A8w9g/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwpgUK53XiJxYo8B9wWtEa0hM_ZYdwziwIijgKOtyZ_Yrawnr3tAEm9vdflzKzrAeMPgQ/exec",
 
   // Si la URL está vacía, la app usa datos locales para probar todo el CRUD.
   USE_DEMO_DATA_WHEN_EMPTY: true,
