0ecc92dfebaf9ad35bd560a7071d272a51d307e5
e <e@e>
2026-07-25 11:43:24 -0500

    Conectar despliegue con corrección de teléfonos

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.ATELIER_CONFIG = {
   // Pega aquí la URL /exec de tu despliegue de Google Apps Script.
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzWjpR_ZNmdvMVibXWmdxU3wyhVwGGaFSUrpfbj4wmR7-PAoVBIGovyMnOM96waUN8x1w/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyIlB2Fdi7SMwd0hB_14hUx1vkNKnaeMP-7v3nowhd8uCBIls-cBNiaMQrCIhk4kgCWzQ/exec",
 
   // Si la URL está vacía, la app usa datos locales para probar todo el CRUD.
   USE_DEMO_DATA_WHEN_EMPTY: true,
